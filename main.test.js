bbfc5619db4b1ace110dd87f430d2a5cddf0e883
Travis Lane <coderlane@gmail.com>
2020-05-18 21:49:06 -0400

    Enable Code Coverage
    Grab the code coverage report with Jest

--- main.test.js
+++ main.test.js
@@ -1,8 +1,7 @@
-const process = require('process');
 const cp = require('child_process');
 const path = require('path');
 
 test('test runs', () => {
-    const ip = path.join(__dirname, 'main.js');
-    console.log(cp.execSync(`node ${ip}`).toString());
-})
+  const ip = path.join(__dirname, 'main.js');
+  console.log(cp.execSync(`node ${ip}`).toString());
+});
